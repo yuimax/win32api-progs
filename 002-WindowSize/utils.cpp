a12f13168a5f5b1a7932ee7986b8c14035aa68a3
yuimax <vimalakitty@yuimax.org>
2026-09-13 02:07:26 +0900

    cmt 2026/09/13 02:07 (UTC+9)

--- 002-WindowSize/utils.cpp
+++ 002-WindowSize/utils.cpp
@@ -1,54 +1,41 @@
 #include "utils.h"
 
-// EXEファイルと同じディレクトリで、ファイル名の拡張子を ".ini" に変えたパスを得る
-static std::wstring GetIniFilePath()
-{
-	WCHAR exePath[MAX_PATH];
-	GetModuleFileName(NULL, exePath, MAX_PATH);
-
-	WCHAR drive[_MAX_DRIVE];
-	WCHAR dir[_MAX_DIR];
-	WCHAR fname[_MAX_FNAME];
-	WCHAR ext[_MAX_EXT];
-	_wsplitpath_s(exePath,
-		drive, _MAX_DRIVE,
-		dir, _MAX_DIR,
-		fname, _MAX_FNAME,
-		ext, _MAX_EXT);
-
-	WCHAR iniFilePath[MAX_PATH];
-	swprintf_s(iniFilePath, L"%s%s%s.ini", drive, dir, fname);
-
-	return std::wstring(iniFilePath);
-}
-
 // INIファイルのパス
-static std::wstring MyIniFilePath = GetIniFilePath();
+static WCHAR IniFilePath[MAX_PATH];
+
+// INIファイルのメインセクション名
+static WCHAR MainSection[] = L"MainWindow";
+
+// WinMain() が実行される前に IniFilePath[] を初期化する
+struct Initializer {
+	Initializer() {
+		WCHAR exePath[MAX_PATH];
+		GetModuleFileName(NULL, exePath, MAX_PATH);
+
+		WCHAR drive[_MAX_DRIVE];
+		WCHAR dir[_MAX_DIR];
+		WCHAR fname[_MAX_FNAME];
+		WCHAR ext[_MAX_EXT];
+		_wsplitpath_s(exePath, drive, _MAX_DRIVE, dir, _MAX_DIR, fname, _MAX_FNAME, ext, _MAX_EXT);
+		swprintf_s(IniFilePath, L"%s%s%s.ini", drive, dir, fname);
+	}
+};
+
+static Initializer initializer;
 
 // 整数を保存する
 static void SaveInt(LPCWSTR section, LPCWSTR key, int value)
 {
-	WritePrivateProfileStringW(
-		section,
-		key,
-		std::to_wstring(value).c_str(),
-		MyIniFilePath.c_str()
-	);
+	WCHAR buffer[50];	// 整数の文字列化に十分な長さを確保
+	swprintf_s(buffer, L"%d", value);
+	WritePrivateProfileString(section, key, buffer, IniFilePath);
 }
 
 // 整数を復元する
 static int LoadInt(LPCWSTR section, LPCWSTR key, int defaultValue)
 {
-	return GetPrivateProfileIntW(
-		section,
-		key,
-		defaultValue,
-		MyIniFilePath.c_str()
-	);
+	return GetPrivateProfileInt(section, key, defaultValue, IniFilePath);
 }
-
-// 設定ファイルのセクション名
-static const LPCWSTR MainSection = L"MainWindow";
 
 // 設定の保存
 void SaveSettings(HWND hWnd)
@@ -82,6 +69,6 @@ void MyDrawText(HDC hdc, int x, int y, LPCWSTR text)
 	// メモ：
 	//	第2引数のtextは改行コードが有効（TextOut() だと改行できない）
 	//	第3引数は文字数で、ここに-1を指定すると自動計算する
-	//	第4引数は表示範囲で、書式に DT_NOCLIP を指定する場合は rc.right と rc.bottom を無視
+	//	第4引数は表示範囲で、書式が DT_NOCLIP の場合は rc.right と rc.bottom を無視
 	//	第5引数は書式で、右詰めやセンタリングなどいろいろ指定できる
 }
--- 002-WindowSize/utils.h
+++ 002-WindowSize/utils.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <windows.h>
-#include <string>
+#include <stdio.h>	// for swprintf_s()
 
 extern void LoadSettings(HWND hWnd);
 extern void SaveSettings(HWND hWnd);

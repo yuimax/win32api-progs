c677dbb31a41d0b1cfe3d9f2160878e777c7b3f0
yuimax <vimalakitty@yuimax.org>
2026-09-16 12:43:28 +0900

    cmt 2026/09/16 12:43 (UTC+9)

--- 001-MainWindow/001-MainWindow.cpp
+++ 001-MainWindow/001-MainWindow.cpp
@@ -8,36 +8,41 @@ constexpr int INIT_WINDOW_HEIGHT = 480;
 
 // グローバル変数
 HINSTANCE hInst;
-WCHAR szTitle[] = L"win32api 001-MainWindow";
-WCHAR szWindowClass[] = L"WinClass 001-MainWindow";
+WCHAR WindowTitle[] = L"win32api 001-MainWindow";
+WCHAR WindowClassName[] = L"WinClass 001-MainWindow";
 
 // プロトタイプ宣言
 ATOM MyRegisterClass(HINSTANCE hInstance);
 BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-// x64用エントリポイント
+// エントリポイント
 int APIENTRY wWinMain(
 	_In_     HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_     LPWSTR lpCmdLine,
 	_In_     int nCmdShow)
 {
-	hInst = hInstance;		// あとで使うのでグローバル変数に格納
-	(void)hPrevInstance;	// 引数未使用の警告を回避
-	(void)lpCmdLine;		// 引数未使用の警告を回避
+	// 引数未使用の警告を回避
+	(void)hPrevInstance;
+	(void)lpCmdLine;
 
+	// ウィンドウクラスを登録する
 	MyRegisterClass(hInstance);
 
+	// ウィンドウを作成する
 	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
 
+	// hInstanceをグローバル変数に保存しておく
+	hInst = hInstance;
+
+	// メッセージループ
 	MSG msg;
 	while (GetMessage(&msg, nullptr, 0, 0))
 	{
-		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
@@ -47,40 +52,39 @@ int APIENTRY wWinMain(
 // メインウィンドウのウィンドウクラスを登録
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-	WNDCLASSEXW wcex;
+	WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
 
-	wcex.cbSize = sizeof(WNDCLASSEX);
 	wcex.style = CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
-	wcex.hCursor = nullptr; // カーソルを自分で設定するので、nullptrにする
-	wcex.hbrBackground = nullptr; // 背景を自分で描画するので、nullptrにする
-	wcex.lpszMenuName = nullptr; // メニューは使用しないので、nullptrにする
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = nullptr; // 小さいアイコンにも hIcon を兼用するので、nullptrにする
+	wcex.hIcon = nullptr;			// アイコンがデフォルトでよければ、nullptrにする
+	wcex.hCursor = nullptr;			// カーソルを自分で設定するなら、nullptrにする
+	wcex.hbrBackground = nullptr;	// 背景を自分で描画するなら、nullptrにする
+	wcex.lpszMenuName = nullptr;	// メニューを自分で用意するなら、nullptrにする
+	wcex.lpszClassName = WindowClassName;
+	wcex.hIconSm = nullptr;			// hIcon と同じでよい場合は、nullptrにする
 
-	return RegisterClassExW(&wcex);
+	return RegisterClassEx(&wcex);
 }
 
 // メインウィンドウの作成
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-	HWND hWnd = CreateWindowExW(
-		0,                   // dwExStyle
-		szWindowClass,       // lpClassName
-		szTitle,             // lpWindowName
-		WS_OVERLAPPEDWINDOW, // dwStyle
-		CW_USEDEFAULT,       // X
-		CW_USEDEFAULT,       // Y
-		INIT_WINDOW_WIDTH,   // nWidth
-		INIT_WINDOW_HEIGHT,  // nHeight
-		nullptr,             // hWndParent
-		nullptr,             // hMenu
-		hInstance,           // hInstance
-		nullptr              // lpParam
+	HWND hWnd = CreateWindowEx(
+		0,						// dwExStyle
+		WindowClassName,		// lpClassName
+		WindowTitle,			// lpWindowName
+		WS_OVERLAPPEDWINDOW,	// dwStyle
+		CW_USEDEFAULT,			// X
+		CW_USEDEFAULT,			// Y
+		INIT_WINDOW_WIDTH,		// nWidth
+		INIT_WINDOW_HEIGHT,		// nHeight
+		nullptr,				// hWndParent
+		nullptr,				// hMenu
+		hInstance,				// hInstance
+		nullptr					// lpParam
 	);
 
 	if (!hWnd)
@@ -102,29 +106,54 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	case WM_PAINT:
 	{
+		// 画面を更新すべきタイミングでこのメッセージが来る
+
+		// 前準備
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
+
+		// 背景を塗りつぶす
 		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+		// テキストを表示
+		WCHAR text[100];
+		MySprintf(text, L"Hello world!");
+		MyTextOut(hdc, 10, 10, text);
+
+		// 後始末
 		EndPaint(hWnd, &ps);
 		break;
 	}
 
 	case WM_SETCURSOR:
 	{
+		// マウスカーソルを変えるべきタイミングでこのメッセージが来る
+
+		// マウスカーソルの下に何があるかチェック
 		WORD hitTest = LOWORD(lParam);
+
+		// クライアント領域の場合は、標準の矢印カーソルにする
 		if (hitTest == HTCLIENT)
 		{
 			HCURSOR hCursor = LoadCursor(0, IDC_ARROW);
 			SetCursor(hCursor);
 			return TRUE;	// 自分でカーソルを設定した場合はTRUEを返す
 		}
+
+		// それ以外（枠線上など）の場合は、デフォルト処理にまかせる
 		break;
 	}
 
 	case WM_DESTROY:
+	{
+		// ウィンドウが破棄されたとき、このメッセージが来る
+
+		// メッセージループを終了する
 		PostQuitMessage(0);
 		break;
 	}
+
+	} // end of switch (message)
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
--- 001-MainWindow/utils.cpp
+++ 001-MainWindow/utils.cpp
@@ -1,3 +1,17 @@
-#include "utils.h"
+#include <windows.h>
 
-// このファイルにユーティリティ関数などを追加する
+// ウィンドウにテキストを表示する
+// テキストに改行(\n)を含めることができる
+void MyTextOut(HDC hdc, int x, int y, LPCWSTR text)
+{
+	RECT rc = { x, y, 0, 0 }; // left, top, right, bottom
+	DrawText(hdc, text, -1, &rc, DT_NOCLIP | DT_NOPREFIX);
+
+	// メモ： DrawText()について
+	//	第2引数のtext内では改行コード(\n)が有効
+	//	またtext内の文字'&'は「次の文字を下線で修飾する」という意味になる
+	//	'&'を普通の文字として表示するには、書式に DT_NOPREFIX を含める
+	//	第3引数は文字数で、-1を指定すると自動計算する
+	//	第4引数は表示範囲で、書式に DT_NOCLIP がある場合は rc.right と rc.bottom を無視
+	//	第5引数は書式で、右詰めやセンタリングなどいろいろ
+}
